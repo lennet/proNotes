b72e4202446c5a52f3d4b6cbf7395ec5fc7bb1e7
Leonard Thomas <leo.thomas@me.com>
2015-11-26 20:51:19 +0100

    init project

---
+++ Student/Student/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Student
+//
+//  Created by Leo Thomas on 26/11/15.
+//  Copyright © 2015 leonardthomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
