5c1a2864de7743ef2795cd798a371828da9819a6
Leonard Thomas <leo.thomas@me.com>
2016-02-11 23:23:09 +0100

    fixed script location #2

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+cd Student/
+
 TEST_CMD="xcodebuild -scheme Student -project Student.xcodeproj -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' build test"
 
 which -s xcpretty
