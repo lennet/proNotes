eaff4ddd346ce71c8cc8800bc04a487a3bf625cc
Leonard Thomas <leo.thomas@me.com>
2016-02-11 23:16:35 +0100

    changed travis language to objective c

---
+++ test.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+TEST_CMD="xcodebuild -scheme Student -project Student.xcodeproj -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' build test"
+
+which -s xcpretty
+XCPRETTY_INSTALLED=$?
+
+if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
+	eval "${TEST_CMD} | xcpretty"
+else
+	eval "$TEST_CMD"
+fi
