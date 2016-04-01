ea5aa0903fefd441fa1dcd892585c6a33974a93d
Leonard Thomas <leo.thomas@me.com>
2016-04-01 19:43:05 +0200

    refactored reordabletableview

--- app/proNotes/Generic Components/ReordableTableView.swift
+++ app/proNotes/Generic Components/ReordableTableView.swift
@@ -14,74 +14,88 @@ protocol ReordableTableViewDelegate: class {
 
 class ReordableTableView: UITableView {
 
-    var currentSnapShowView: UIView = UIView()
-    var sourceIndexPath: NSIndexPath?
-
     weak var reordableDelegate: ReordableTableViewDelegate?
+    
+    private var sourceIndexPath: NSIndexPath?
+    private weak var currentSnapShowView: UIView?
 
     override func awakeFromNib() {
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(ReordableTableView.handleLongPress(_:)))
         addGestureRecognizer(longPressRecognizer)
     }
 
-    // Inspired by http://www.raywenderlich.com/63089/cookbook-moving-table-view-cells-with-a-long-press-gesture
     func handleLongPress(gestureRecognizer: UILongPressGestureRecognizer) {
         let location = gestureRecognizer.locationInView(self)
-        guard let indexPath = indexPathForRowAtPoint(location) else {
-            return
-        }
-
         switch gestureRecognizer.state {
         case .Began:
-            sourceIndexPath = indexPath
-            guard let cell = cellForRowAtIndexPath(indexPath) else {
-                return
-            }
-            currentSnapShowView = cell.toImageView()
-            var center = cell.center
-            currentSnapShowView.center = center
-            currentSnapShowView.alpha = 0
-            addSubview(currentSnapShowView)
-            UIView.animateWithDuration(standardAnimationDuration, animations: {
-                () -> Void in
-                center.y = location.y
-                self.currentSnapShowView.center = center
-                self.currentSnapShowView.transform = CGAffineTransformMakeScale(1.05, 1.05)
-                self.currentSnapShowView.alpha = 0.98
-                cell.alpha = 0
-            }, completion: {
-                (Bool) -> Void in
-                cell.hidden = true
-            })
+            handleTouchBegan(location)
             break
         case .Changed:
-            var center = currentSnapShowView.center
-            center.y = location.y
-            currentSnapShowView.center = center
-            if !indexPath.isEqual(sourceIndexPath) {
-                reordableDelegate?.didSwapElements((sourceIndexPath?.row)!, secondIndex: indexPath.row)
-                moveRowAtIndexPath(sourceIndexPath!, toIndexPath: indexPath)
-                sourceIndexPath = indexPath
-            }
+            handleTouchChanged(location)
             break
         default:
-            let cell = cellForRowAtIndexPath(sourceIndexPath!)
-            cell?.hidden = false
-            cell?.alpha = 0
-            UIView.animateWithDuration(standardAnimationDuration, animations: {
-                () -> Void in
-                self.currentSnapShowView.center = cell?.center ?? self.currentSnapShowView.center
-                self.currentSnapShowView.transform = CGAffineTransformIdentity
-                self.currentSnapShowView.alpha = 0
-                cell?.alpha = 1
-            }, completion: {
-                (Bool) -> Void in
-                self.sourceIndexPath = nil
-                self.currentSnapShowView.removeFromSuperview()
-                self.currentSnapShowView = UIView()
-            })
-            reloadData()
+            handleTouchEnded()
             break
         }
     }
+    
+    private func handleTouchBegan(touchLocation: CGPoint) {
+        guard let indexPath = indexPathForRowAtPoint(touchLocation) else {
+            return
+        }
+        
+        guard let cell = cellForRowAtIndexPath(indexPath) else {
+            return
+        }
+        let currentSnapShowView = cell.toImageView()
+        currentSnapShowView.center = cell.center
+        currentSnapShowView.alpha = 0
+        addSubview(currentSnapShowView)
+        self.currentSnapShowView = currentSnapShowView
+        UIView.animateWithDuration(standardAnimationDuration, animations: {
+            () -> Void in
+            self.currentSnapShowView?.transform = CGAffineTransformMakeScale(1.05, 1.05)
+            self.currentSnapShowView?.alpha = 0.98
+            cell.alpha = 0
+            }, completion: {
+                (Bool) -> Void in
+                cell.hidden = true
+        })
+        sourceIndexPath = indexPath
+    }
+    
+    private func handleTouchChanged(touchLocation: CGPoint) {
+        currentSnapShowView?.center.y = touchLocation.y
+        guard let currentIndexPath = indexPathForRowAtPoint(touchLocation), let oldIndexPath = sourceIndexPath else {
+            return
+        }
+        if !currentIndexPath.isEqual(oldIndexPath) {
+            reordableDelegate?.didSwapElements(oldIndexPath.row, secondIndex: currentIndexPath.row)
+            moveRowAtIndexPath(oldIndexPath, toIndexPath: currentIndexPath)
+            sourceIndexPath = currentIndexPath
+        }
+    }
+    
+    private func handleTouchEnded() {
+        guard let indexPath = sourceIndexPath, let cell = cellForRowAtIndexPath(indexPath) else {
+            return
+        }
+        
+        cell.hidden = false
+        cell.alpha = 0
+        
+        UIView.animateWithDuration(standardAnimationDuration, animations: {
+            () -> Void in
+            self.currentSnapShowView?.center = cell.center
+            self.currentSnapShowView?.transform = CGAffineTransformIdentity
+            self.currentSnapShowView?.alpha = 0
+            cell.alpha = 1
+            }, completion: {
+                (Bool) -> Void in
+                self.sourceIndexPath = nil
+                self.currentSnapShowView?.removeFromSuperview()
+        })
+        reloadData()
+    }
+    
 }
