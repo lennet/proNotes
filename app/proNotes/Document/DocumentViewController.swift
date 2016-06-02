6cc0df89588bbf7bd55be601d01d0168e24015ca
Leonard Thomas <leo.thomas@me.com>
2016-06-02 22:45:14 +0200

    changed documentviewcontroller structure

--- app/proNotes/Document/DocumentViewController.swift
+++ app/proNotes/Document/DocumentViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DocumentViewController: UIViewController, PagesOverviewTableViewCellDelegate, UITextFieldDelegate, ImportExportDataViewControllerDelgate, SettingsViewControllerDelegate {
+class DocumentViewController: UIViewController {
 
     @IBOutlet weak var settingsContainerRightConstraint: NSLayoutConstraint!
     @IBOutlet weak var pagesOverViewLeftConstraint: NSLayoutConstraint!
@@ -22,6 +22,7 @@ class DocumentViewController: UIViewController, PagesOverviewTableViewCellDelega
     @IBOutlet var bottomConstraints: [NSLayoutConstraint]!
 
     weak var pagesOverviewController: PagesOverviewTableViewController?
+    weak var importDataNavigationController: UINavigationController?
     
     var isFullScreen = false
     var isSketchMode = false {
@@ -158,185 +159,6 @@ class DocumentViewController: UIViewController, PagesOverviewTableViewCellDelega
     func updateUndoButton() {
         undoButton.enabled = undoManager?.canUndo ?? false
     }
-
-    // MARK: - PagesOverViewDelegate
-
-    func showPage(index: Int) {
-        PagesTableViewController.sharedInstance?.showPage(index)
-    }
-
-    // MARK: - UIKeyCommands
-
-    override var keyCommands: [UIKeyCommand]? {
-        var commands = [UIKeyCommand]()
-
-        if let settingsViewController = SettingsViewController.sharedInstance {
-            switch settingsViewController.currentSettingsType {
-            case .Image:
-                commands.append(UIKeyCommand(input: UIKeyInputRightArrow, modifierFlags: .Control, action: #selector(DocumentViewController.handleRotateImageKeyPressed(_:)), discoverabilityTitle: "Rotate Image Right"))
-                commands.append(UIKeyCommand(input: UIKeyInputLeftArrow, modifierFlags: .Control, action: #selector(DocumentViewController.handleRotateImageKeyPressed(_:)), discoverabilityTitle: "Rotate Image Left"))
-                break
-            default:
-                break
-            }
-        }
-
-        if let _ = PagesTableViewController.sharedInstance?.currentPageView?.selectedSubView as? MovableView {
-            commands.append(UIKeyCommand(input: UIKeyInputRightArrow, modifierFlags: .Command, action: #selector(DocumentViewController.handleMoveMovableViewKeyPressed(_:)), discoverabilityTitle: "Move Right"))
-            commands.append(UIKeyCommand(input: UIKeyInputLeftArrow, modifierFlags: .Command, action: #selector(DocumentViewController.handleMoveMovableViewKeyPressed(_:)), discoverabilityTitle: "Move Left"))
-            commands.append(UIKeyCommand(input: UIKeyInputUpArrow, modifierFlags: .Command, action: #selector(DocumentViewController.handleMoveMovableViewKeyPressed(_:)), discoverabilityTitle: "Move Up"))
-            commands.append(UIKeyCommand(input: UIKeyInputDownArrow, modifierFlags: .Command, action: #selector(DocumentViewController.handleMoveMovableViewKeyPressed(_:)), discoverabilityTitle: "Move Down"))
-        } else {
-            commands.append(UIKeyCommand(input: UIKeyInputDownArrow, modifierFlags: [], action: #selector(DocumentViewController.handleDownKeyPressed(_:)), discoverabilityTitle: "Scroll Down"))
-            commands.append(UIKeyCommand(input: UIKeyInputUpArrow, modifierFlags: [], action: #selector(DocumentViewController.handleUpKeyPressed(_:)), discoverabilityTitle: "Scroll Up"))
-        }
-
-        return commands
-    }
-
-    func handleRotateImageKeyPressed(sender: UIKeyCommand) {
-        if let imageSettingsViewController = SettingsViewController.sharedInstance?.currentChildViewController as? ImageSettingsViewController {
-            imageSettingsViewController.rotateImage(sender.input == UIKeyInputRightArrow ? .Right : .Left)
-        }
-    }
-
-    func handleDownKeyPressed(sender: UIKeyCommand) {
-        PagesTableViewController.sharedInstance?.scroll(true)
-    }
-
-    func handleUpKeyPressed(sender: UIKeyCommand) {
-        PagesTableViewController.sharedInstance?.scroll(false)
-    }
-
-    func handleMoveMovableViewKeyPressed(sender: UIKeyCommand) {
-        guard let movableView = PagesTableViewController.sharedInstance?.currentPageView?.selectedSubView as? MovableView else {
-            return
-        }
-        let offSet = 10
-        var translation = CGPointZero
-        switch sender.input {
-        case UIKeyInputRightArrow:
-            translation = CGPoint(x: offSet, y: 0)
-            break
-        case UIKeyInputLeftArrow:
-            translation = CGPoint(x: -offSet, y: 0)
-            break
-        case UIKeyInputDownArrow:
-            translation = CGPoint(x: 0, y: offSet)
-            break
-        case UIKeyInputUpArrow:
-            translation = CGPoint(x: 0, y: -offSet)
-            break
-        default:
-            break
-        }
-        movableView.selectedTouchControl = .Center
-        movableView.handlePanTranslation(translation)
-        movableView.handlePanEnded()
-    }
-
-    // MARK: - UITextfieldDelegate
-
-    func textFieldDidBeginEditing(textField: UITextField) {
-        textField.borderStyle = .RoundedRect
-    }
-
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-
-    func textFieldDidEndEditing(textField: UITextField) {
-        if let newName = textField.text {
-            DocumentInstance.sharedInstance.renameDocument(newName, forceOverWrite: false, viewController: self, completion: {
-                (success) -> Void in
-                if !success {
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.setUpTitle()
-                    })
-                }
-            })
-            textField.borderStyle = .None
-        }
-    }
-
-    // MARK: - ImportDataViewControllerDelegate
-    
-    weak var importDataNavigationController: UINavigationController?
-
-    func addEmptyPage() {
-        document?.addEmptyPage()
-        dismiss()
-    }
-
-    func addTextField() {
-        if let textLayer = DocumentInstance.sharedInstance.currentPage?.addTextLayer("") {
-            if let currentPageView = PagesTableViewController.sharedInstance?.currentPageView {
-                currentPageView.addTextLayer(textLayer)
-                currentPageView.page = DocumentInstance.sharedInstance.currentPage
-                currentPageView.setLayerSelected(currentPageView.subviews.count - 1)
-                if let pageIndex = currentPageView.page?.index {
-                    DocumentInstance.sharedInstance.didUpdatePage(pageIndex)
-                    showPage(pageIndex)
-                }
-            }
-        }
-        dismiss()
-    }
-
-    func addPDF(url: NSURL) {
-        document?.addPDF(url)
-        dismiss()
-    }
-
-    func addImage(image: UIImage) {
-        if let imageLayer = DocumentInstance.sharedInstance.currentPage?.addImageLayer(image) {
-            if let currentPageView = PagesTableViewController.sharedInstance?.currentPageView {
-                currentPageView.addImageLayer(imageLayer)
-                currentPageView.page = DocumentInstance.sharedInstance.currentPage
-                currentPageView.setLayerSelected(currentPageView.subviews.count - 1)
-                if let pageIndex = currentPageView.page?.index {
-                    DocumentInstance.sharedInstance.didUpdatePage(pageIndex)
-                    showPage(pageIndex)
-                }
-            }
-        }
-        dismiss()
-    }
-    
-    
-    func addSketchLayer() {
-        PagesTableViewController.sharedInstance?.currentPageView?.addSketchLayer()
-        dismiss()
-    }
-    
-    func exportAsPDF(data: NSData) {
-        dismiss()
-        DocumentExporter.presentActivityViewController(self, barbuttonItem: actionBarButtonItem, items: [data])
-    }
-    
-    func exportAsImages(images: [UIImage]) {
-        dismiss(false)
-        DocumentExporter.presentActivityViewController(self, barbuttonItem: actionBarButtonItem, items: images)
-    }
-    
-    func exportAsProNote(url: NSURL) {
-        dismiss()
-        DocumentExporter.presentActivityViewController(self, barbuttonItem: actionBarButtonItem, items: [url])
-    }
-
-    func dismiss(animated: Bool = true) {
-        importDataNavigationController?.dismissViewControllerAnimated(animated, completion: nil)
-        importDataNavigationController?.delegate = nil
-        importDataNavigationController = nil
-    }
-    
-    // MARK: - SettingsViewControllerDelegate
-    
-    func didChangeSettingsType(newType: SettingsViewControllerType) {
-        pageInfoButton.setHidden(newType == .PageInfo)
-        isSketchMode = newType == .Sketch
-    }
     
     // MARK: - Navigation
     
@@ -364,5 +186,200 @@ class DocumentViewController: UIViewController, PagesOverviewTableViewCellDelega
     
     @IBAction func unwind(sender: AnyObject) {
         navigationController?.popViewControllerAnimated(true)
+    }
+}
+
+// MARK: - UITextfieldDelegate
+
+extension DocumentViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.borderStyle = .RoundedRect
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if let newName = textField.text {
+            DocumentInstance.sharedInstance.renameDocument(newName, forceOverWrite: false, viewController: self, completion: {
+                (success) -> Void in
+                if !success {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.setUpTitle()
+                    })
+                }
+            })
+            textField.borderStyle = .None
+        }
+    }
+    
+}
+
+// MARK: - SettingsViewControllerDelegate
+
+extension DocumentViewController: SettingsViewControllerDelegate {
+    
+    func didChangeSettingsType(newType: SettingsViewControllerType) {
+        pageInfoButton.setHidden(newType == .PageInfo)
+        isSketchMode = newType == .Sketch
+    }
+    
+}
+
+// MARK: - ImportDataViewControllerDelegate
+
+extension DocumentViewController: ImportExportDataViewControllerDelgate {
+    
+    func addEmptyPage() {
+        document?.addEmptyPage()
+        dismiss()
+    }
+    
+    func addTextField() {
+        if let textLayer = DocumentInstance.sharedInstance.currentPage?.addTextLayer("") {
+            if let currentPageView = PagesTableViewController.sharedInstance?.currentPageView {
+                currentPageView.addTextLayer(textLayer)
+                currentPageView.page = DocumentInstance.sharedInstance.currentPage
+                currentPageView.setLayerSelected(currentPageView.subviews.count - 1)
+                if let pageIndex = currentPageView.page?.index {
+                    DocumentInstance.sharedInstance.didUpdatePage(pageIndex)
+                    showPage(pageIndex)
+                }
+            }
+        }
+        dismiss()
+    }
+    
+    func addPDF(url: NSURL) {
+        document?.addPDF(url)
+        dismiss()
+    }
+    
+    func addImage(image: UIImage) {
+        if let imageLayer = DocumentInstance.sharedInstance.currentPage?.addImageLayer(image) {
+            if let currentPageView = PagesTableViewController.sharedInstance?.currentPageView {
+                currentPageView.addImageLayer(imageLayer)
+                currentPageView.page = DocumentInstance.sharedInstance.currentPage
+                currentPageView.setLayerSelected(currentPageView.subviews.count - 1)
+                if let pageIndex = currentPageView.page?.index {
+                    DocumentInstance.sharedInstance.didUpdatePage(pageIndex)
+                    showPage(pageIndex)
+                }
+            }
+        }
+        dismiss()
+    }
+    
+    func addSketchLayer() {
+        PagesTableViewController.sharedInstance?.currentPageView?.addSketchLayer()
+        dismiss()
+    }
+    
+    func exportAsPDF(data: NSData) {
+        dismiss()
+        DocumentExporter.presentActivityViewController(self, barbuttonItem: actionBarButtonItem, items: [data])
+    }
+    
+    func exportAsImages(images: [UIImage]) {
+        dismiss(false)
+        DocumentExporter.presentActivityViewController(self, barbuttonItem: actionBarButtonItem, items: images)
+    }
+    
+    func exportAsProNote(url: NSURL) {
+        dismiss()
+        DocumentExporter.presentActivityViewController(self, barbuttonItem: actionBarButtonItem, items: [url])
+    }
+    
+    func dismiss(animated: Bool = true) {
+        importDataNavigationController?.dismissViewControllerAnimated(animated, completion: nil)
+        importDataNavigationController?.delegate = nil
+        importDataNavigationController = nil
+    }
+    
+}
+
+// MARK: - PagesOverViewDelegate
+
+extension DocumentViewController: PagesOverviewTableViewCellDelegate {
+
+    func showPage(index: Int) {
+        PagesTableViewController.sharedInstance?.showPage(index)
+    }
+
+}
+
+// MARK: - UIKeyCommands
+
+extension DocumentViewController {
+    
+    override var keyCommands: [UIKeyCommand]? {
+        var commands = [UIKeyCommand]()
+        
+        if let settingsViewController = SettingsViewController.sharedInstance {
+            switch settingsViewController.currentSettingsType {
+            case .Image:
+                commands.append(UIKeyCommand(input: UIKeyInputRightArrow, modifierFlags: .Control, action: #selector(DocumentViewController.handleRotateImageKeyPressed(_:)), discoverabilityTitle: "Rotate Image Right"))
+                commands.append(UIKeyCommand(input: UIKeyInputLeftArrow, modifierFlags: .Control, action: #selector(DocumentViewController.handleRotateImageKeyPressed(_:)), discoverabilityTitle: "Rotate Image Left"))
+                break
+            default:
+                break
+            }
+        }
+        
+        if let _ = PagesTableViewController.sharedInstance?.currentPageView?.selectedSubView as? MovableView {
+            commands.append(UIKeyCommand(input: UIKeyInputRightArrow, modifierFlags: .Command, action: #selector(DocumentViewController.handleMoveMovableViewKeyPressed(_:)), discoverabilityTitle: "Move Right"))
+            commands.append(UIKeyCommand(input: UIKeyInputLeftArrow, modifierFlags: .Command, action: #selector(DocumentViewController.handleMoveMovableViewKeyPressed(_:)), discoverabilityTitle: "Move Left"))
+            commands.append(UIKeyCommand(input: UIKeyInputUpArrow, modifierFlags: .Command, action: #selector(DocumentViewController.handleMoveMovableViewKeyPressed(_:)), discoverabilityTitle: "Move Up"))
+            commands.append(UIKeyCommand(input: UIKeyInputDownArrow, modifierFlags: .Command, action: #selector(DocumentViewController.handleMoveMovableViewKeyPressed(_:)), discoverabilityTitle: "Move Down"))
+        } else {
+            commands.append(UIKeyCommand(input: UIKeyInputDownArrow, modifierFlags: [], action: #selector(DocumentViewController.handleDownKeyPressed(_:)), discoverabilityTitle: "Scroll Down"))
+            commands.append(UIKeyCommand(input: UIKeyInputUpArrow, modifierFlags: [], action: #selector(DocumentViewController.handleUpKeyPressed(_:)), discoverabilityTitle: "Scroll Up"))
+        }
+        
+        return commands
+    }
+    
+    func handleRotateImageKeyPressed(sender: UIKeyCommand) {
+        if let imageSettingsViewController = SettingsViewController.sharedInstance?.currentChildViewController as? ImageSettingsViewController {
+            imageSettingsViewController.rotateImage(sender.input == UIKeyInputRightArrow ? .Right : .Left)
+        }
+    }
+    
+    func handleDownKeyPressed(sender: UIKeyCommand) {
+        PagesTableViewController.sharedInstance?.scroll(true)
+    }
+    
+    func handleUpKeyPressed(sender: UIKeyCommand) {
+        PagesTableViewController.sharedInstance?.scroll(false)
+    }
+    
+    func handleMoveMovableViewKeyPressed(sender: UIKeyCommand) {
+        guard let movableView = PagesTableViewController.sharedInstance?.currentPageView?.selectedSubView as? MovableView else {
+            return
+        }
+        let offSet = 10
+        var translation = CGPointZero
+        switch sender.input {
+        case UIKeyInputRightArrow:
+            translation = CGPoint(x: offSet, y: 0)
+            break
+        case UIKeyInputLeftArrow:
+            translation = CGPoint(x: -offSet, y: 0)
+            break
+        case UIKeyInputDownArrow:
+            translation = CGPoint(x: 0, y: offSet)
+            break
+        case UIKeyInputUpArrow:
+            translation = CGPoint(x: 0, y: -offSet)
+            break
+        default:
+            break
+        }
+        movableView.selectedTouchControl = .Center
+        movableView.handlePanTranslation(translation)
+        movableView.handlePanEnded()
     }
 }
