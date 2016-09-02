3be9bf78c69922c6896ffb5884589c3c0470bd22
Leo Thomas <leo.thomas@me.com>
2016-09-02 22:39:50 +0200

    removed forced unwrapping

--- app/proNotes/Utilities/DocumentExporter.swift
+++ app/proNotes/Utilities/DocumentExporter.swift
@@ -8,37 +8,39 @@
 
 import UIKit
 
-class DocumentExporter: NSObject {
-
-    class func exportAsImages(_ progress: (Float) -> Void) -> [UIImage]{
+class DocumentExporter {
+    
+    class func exportAsImages(_ progress: (Float) -> Void) -> [UIImage] {
         guard let document = DocumentInstance.sharedInstance.document else {
             return []
         }
         let images = getImageArrayForDocument(document, progress: progress)
         return images
     }
-
+    
     class func exportAsPDF(_ progress: (Float) -> Void) -> Data? {
-            guard let document = DocumentInstance.sharedInstance.document else {
-                return nil
-            }
-            let mutableData = NSMutableData()
-            let rect = CGRect(origin: .zero, size: document.pages.first!.size)
-            UIGraphicsBeginPDFContextToData(mutableData, rect, nil)
-            let context = UIGraphicsGetCurrentContext()
-            for image in getImageArrayForDocument(document, progress: progress) {
-                let imageRect = CGRect(origin: .zero, size: image.size)
-                UIGraphicsBeginPDFPageWithInfo(imageRect, nil)
-                context?.draw(image.cgImage!, in: imageRect)
-            }
-            UIGraphicsEndPDFContext()
-            return mutableData as Data
+        guard let document = DocumentInstance.sharedInstance.document,
+            let size = document.pages.first?.size else {
+            return nil
+        }
+        let mutableData = NSMutableData()
+        
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginPDFContextToData(mutableData, rect, nil)
+        let context = UIGraphicsGetCurrentContext()
+        for image in getImageArrayForDocument(document, progress: progress) {
+            let imageRect = CGRect(origin: .zero, size: image.size)
+            UIGraphicsBeginPDFPageWithInfo(imageRect, nil)
+            context?.draw(image.cgImage!, in: imageRect)
+        }
+        UIGraphicsEndPDFContext()
+        return mutableData as Data
     }
-
+    
     class func exportAsProNote(_ progress:  @escaping (Float) -> Void, url: @escaping (URL?) -> Void) {
         progress(0.5)
         DocumentInstance.sharedInstance.save({ (_) in
-        progress(1)
+            progress(1)
             guard let document = DocumentInstance.sharedInstance.document else {
                 url(nil)
                 return
@@ -59,7 +61,7 @@ class DocumentExporter: NSObject {
         return images
     }
     
-    class func presentActivityViewController(_ viewController: UIViewController, barbuttonItem: UIBarButtonItem?, items: [Any] ) {
+    class func presentActivityViewController(_ viewController: UIViewController, barbuttonItem: UIBarButtonItem?, items: [Any]) {
         let activityViewController = UIActivityViewController(activityItems: items, applicationActivities: nil)
         activityViewController.popoverPresentationController?.barButtonItem = barbuttonItem
         viewController.present(activityViewController, animated: true, completion: nil)
